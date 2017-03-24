db4eb17bb819978e78e66b84af005acd8aaee265
grturner <dafiremaster@gmail.com>
2017-03-24 14:30:46 -0400

    Pulling data from Facebook is working now.

--- app/src/main/java/ftcc/initech/triadareaultimate/fragment/NewsFragment.java
+++ app/src/main/java/ftcc/initech/triadareaultimate/fragment/NewsFragment.java
@@ -8,7 +8,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import ftcc.initech.triadareaultimate.R;
 import ftcc.initech.triadareaultimate.controller.NewsFeed;
@@ -18,19 +25,41 @@ import ftcc.initech.triadareaultimate.controller.NewsFeed;
  */
 
 public class NewsFragment extends Fragment {
-    private NewsFeed newsFeed;
-    private TextView textView;
-    private JSONArray postArray;
+    private NewsFeed mNewsFeed;
+    private TextView mTextView;
+    private JSONArray mPostArray;
+    private JSONObject mFeedResponse;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_news, container, false);
-        newsFeed = new NewsFeed();
-        postArray = newsFeed.getNewsFeed();
-        textView = (TextView) view.getRootView().findViewById(R.id.news_text_view);
+        Bundle params = new Bundle();
+        params.putString("fields", "message,created_time,id,full_picture");
+        params.putString("limit", "20");
+        params.putString("access_token", "1578803498826781|qNW39As051UT6x3tUVGivbUTfR4");
+        //mNewsFeed = new NewsFeed();
+        //mPostArray = mNewsFeed.getNewsFeed();
+        mTextView = (TextView) view.getRootView().findViewById(R.id.news_text_view);
+        new GraphRequest(
+                AccessToken.getCurrentAccessToken(),
+                "/86086823308/feed",
+                params,
+                HttpMethod.GET,
+                new GraphRequest.Callback() {
+                    public void onCompleted(GraphResponse response) {
+                        //System.out.println("Facebook response: " + String.valueOf(response.getJSONArray()));
+                        try {
+                            mFeedResponse = new JSONObject(String.valueOf(response.getJSONObject()));
+                            mTextView.setText(mFeedResponse.toString());
+                        }
+                        catch (JSONException jEx) {
+                            jEx.printStackTrace();
+                        }
+                    }
+                }
+        ).executeAsync();
 
         //Testing
-        textView.setText(postArray.toString());
 
         return view;
     }
