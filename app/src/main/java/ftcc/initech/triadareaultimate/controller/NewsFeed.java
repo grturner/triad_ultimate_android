0f1e24f74b79dd16349b79e13121c8a1278ca86f
grturner <dafiremaster@gmail.com>
2017-03-27 14:50:18 -0400

    Facebook JSON converts to java structure and bound to ListView.

--- app/src/main/java/ftcc/initech/triadareaultimate/controller/NewsFeed.java
+++ app/src/main/java/ftcc/initech/triadareaultimate/controller/NewsFeed.java
@@ -1,39 +1,61 @@
 package ftcc.initech.triadareaultimate.controller;
 
+import android.os.Bundle;
+
 import com.facebook.AccessToken;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.HttpMethod;
 
-import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by grturner on 3/23/2017.
  */
 
+/********************************************************************************
+*       Probably not needed any longer, keeping around for time being           *
+*       GRT - 3/27/17                                                           *
+********************************************************************************/
+
 public class NewsFeed {
-    private JSONArray postArray;
+    private JSONObject mFeedResponse;
 
     // TODO: 3/23/2017 MAKE THIS SHIT WORK 
     public void NewsFeed() {
+        Bundle params = new Bundle();
+        params.putString("fields", "message,created_time,id,full_picture");
+        params.putString("limit", "20");
+        params.putString("access_token", "1578803498826781|qNW39As051UT6x3tUVGivbUTfR4");
         new GraphRequest(
                 AccessToken.getCurrentAccessToken(),
                 "/86086823308/feed",
-                null,
+                params,
                 HttpMethod.GET,
                 new GraphRequest.Callback() {
                     public void onCompleted(GraphResponse response) {
-                        initPostArray(response);
+                        //System.out.println("Facebook response: " + String.valueOf(response.getJSONArray()));
+                        try {
+                            mFeedResponse = new JSONObject(String.valueOf(response.getJSONObject()));
+                        }
+                        catch (JSONException jEx) {
+                            jEx.printStackTrace();
+                        }
                     }
                 }
-        ).executeAndWait();
+        ).executeAsync();
     }
 
-    private void initPostArray(GraphResponse response){
-        postArray = response.getJSONArray();
+    public JSONObject getNewsFeed(){
+        return mFeedResponse;
     }
 
-    public JSONArray getNewsFeed(){
-        return postArray;
+    public Boolean isReady() {
+        if (mFeedResponse == null) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
--- app/src/main/java/ftcc/initech/triadareaultimate/fragment/NewsFragment.java
+++ app/src/main/java/ftcc/initech/triadareaultimate/fragment/NewsFragment.java
@@ -1,11 +1,13 @@
 package ftcc.initech.triadareaultimate.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewGroupCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.facebook.AccessToken;
@@ -17,29 +19,27 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+
 import ftcc.initech.triadareaultimate.R;
-import ftcc.initech.triadareaultimate.controller.NewsFeed;
+import ftcc.initech.triadareaultimate.model.NewsItem;
 
 /**
  * Created by grturner on 3/23/2017.
  */
 
 public class NewsFragment extends Fragment {
-    private NewsFeed mNewsFeed;
-    private TextView mTextView;
-    private JSONArray mPostArray;
+    private JSONArray mFeedArray;
     private JSONObject mFeedResponse;
+    private NewsItem[] mNewsItems;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_news, container, false);
+        final ListView listView = (ListView) view.findViewById(R.id.feed_list);
         Bundle params = new Bundle();
         params.putString("fields", "message,created_time,id,full_picture");
         params.putString("limit", "20");
         params.putString("access_token", "1578803498826781|qNW39As051UT6x3tUVGivbUTfR4");
-        //mNewsFeed = new NewsFeed();
-        //mPostArray = mNewsFeed.getNewsFeed();
-        mTextView = (TextView) view.getRootView().findViewById(R.id.news_text_view);
         new GraphRequest(
                 AccessToken.getCurrentAccessToken(),
                 "/86086823308/feed",
@@ -47,20 +47,64 @@ public class NewsFragment extends Fragment {
                 HttpMethod.GET,
                 new GraphRequest.Callback() {
                     public void onCompleted(GraphResponse response) {
-                        //System.out.println("Facebook response: " + String.valueOf(response.getJSONArray()));
                         try {
-                            mFeedResponse = new JSONObject(String.valueOf(response.getJSONObject()));
-                            mTextView.setText(mFeedResponse.toString());
+                            mFeedResponse = response.getJSONObject();
+                            mFeedArray = mFeedResponse.getJSONArray("data");
+                            mNewsItems = createArray(mFeedArray);
+                            listView.setAdapter(new FeedListAdapter(NewsFragment.this.getContext(),
+                                    mNewsItems));
                         }
-                        catch (JSONException jEx) {
-                            jEx.printStackTrace();
+                        catch (JSONException ex) {
+                            ex.printStackTrace();
                         }
                     }
                 }
         ).executeAsync();
-
-        //Testing
-
         return view;
+    }
+
+    private static NewsItem[] createArray(JSONArray feedArray){
+        NewsItem[] newsArray = new NewsItem[feedArray.length()];
+        for (int i = 0; i < feedArray.length(); i++){
+            newsArray[i] = new NewsItem();
+            try {
+                JSONObject o = feedArray.getJSONObject(i);
+                newsArray[i].setAuthor("author: " + o.getString("id"));
+                newsArray[i].setDate("timestamp: " + o.getString("created_time"));
+                newsArray[i].setPost("payload: " + o.getString("message"));
+                // TODO: 3/27/2017 handle situation where facebook returns no full_picture
+                newsArray[i].setPicture("avatar: " + o.getString("full_picture"));
+            }
+            catch (JSONException ex) {
+                ex.printStackTrace();
+            }
+        }
+        return newsArray;
+    }
+
+    private class FeedListAdapter extends ArrayAdapter<NewsItem> {
+        // TODO: 3/27/2017 more formatting, convert icon to ImageView
+        public FeedListAdapter(Context context, NewsItem[] newsItems){
+            super(context, 0, newsItems);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent){
+            NewsItem item = getItem(position);
+            if (convertView == null) {
+                convertView = LayoutInflater.from(getContext()).inflate(R.layout.feed_list_item,
+                        parent, false);
+            }
+            TextView author = (TextView) convertView.findViewById(R.id.feed_item_author);
+            TextView icon = (TextView) convertView.findViewById(R.id.feed_item_icon);
+            TextView time = (TextView) convertView.findViewById(R.id.feed_item_timestamp);
+            TextView message = (TextView) convertView.findViewById(R.id.feed_item_message);
+            author.setText(item.getAuthor());
+            icon.setText(item.getPicture());
+            time.setText(item.getDate());
+            message.setText(item.getPost());
+            return convertView;
+        }
+
     }
 }
